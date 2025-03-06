aaf0c16b6d59d8086973d4556f19b5b0ae161e0d
joager paukoski <joagerp@gmail.com>
2025-03-05 21:29:55 -0300

    Ajuste p/ 1 a 100

--- TesteGit_JogoNumeroSecreto/app.js
+++ TesteGit_JogoNumeroSecreto/app.js
@@ -1,10 +1,7 @@
-//let titulo = document.querySelector('h1');
-//titulo.innerHTML = 'Jogo do SecretNumber';
-
-//let paragrafo = document.querySelector('p');
-//paragrafo.innerHTML = 'Escolha um número entre 0 e 9'
-//teste
-
+let listaNumerosSorteados = [];
+let qtdLimiteSorteados = 100;
+let numeroSecreto = RandomNumber();
+let tentativas = 1;
 
 function exibirTexto (tag, texto) {
     let campo = document.querySelector(tag);
@@ -14,18 +11,13 @@ function exibirTexto (tag, texto) {
 
 function mensagemInicial() {
     exibirTexto('h1', 'Jogo do Número Secreto');
-    exibirTexto('p', 'Escolha um número entre 0 e 99');
+    exibirTexto('p', 'Escolha um número entre 1 e 100');
 }
 
 mensagemInicial();
 
-let listaNumerosSorteados = [];
-let qtdLimiteSorteados = 100;
-let numeroSecreto = RandomNumber();
-let tentativas = 1;
-
 function RandomNumber() {
-    let numeroEscolhido = parseInt(Math.random()*100);
+    let numeroEscolhido = parseInt(Math.random()*100+1);
     let qtdElementos = listaNumerosSorteados.length;
     if(qtdElementos == qtdLimiteSorteados){
         listaNumerosSorteados = [];
